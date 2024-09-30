05fc473cc0992d2b061dc5ee9c856e7ed72454d7
IlhanYapici <ilhan-yapici@outlook.fr>
2024-09-30 12:02:37 +0200

    chore(config): configure vite build, path aliases, lerna and commitizen

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+import pkg from './package.json'
+
+const peerDependencies = Object.keys(pkg.peerDependencies)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react(), dts({ tsconfigPath: './tsconfig.app.json' })],
+	resolve: {
+		alias: {
+			'@components': resolve(__dirname, './src/components'),
+			'@utils': resolve(__dirname, './src/utils'),
+			'@types': resolve(__dirname, './src/types'),
+		},
+	},
+	build: {
+		copyPublicDir: false,
+		emptyOutDir: true,
+		cssMinify: false,
+		cssCodeSplit: false,
+		lib: {
+			entry: resolve(__dirname, 'src/index.ts'),
+			name: 'index',
+			formats: ['es'],
+		},
+		rollupOptions: {
+			external: [...peerDependencies, 'react/jsx-runtime'],
+			output: {
+				entryFileNames: '[name].js',
+				assetFileNames: (assetInfo) => {
+					if (assetInfo.name === 'style.css') {
+						return 'index.css'
+					}
+
+					return assetInfo.name ?? ''
+				},
+				globals: {
+					react: 'React',
+					'react-dom': 'React-dom',
+					'react/jsx-runtime': 'react/jsx-runtime',
+				},
+			},
+		},
+	},
 })
