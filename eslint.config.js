3bd4df5b90b867151ff7958b69f0ada3260fced7
IlhanYapici <ilhan-yapici@outlook.fr>
2024-10-07 16:25:26 +0200

    chore(eslint): add jsdoc eslint plugin to format jsdoc comments

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import prettier from 'eslint-config-prettier'
+import jsdoc from 'eslint-plugin-jsdoc'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
@@ -7,7 +8,12 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config({
 	ignores: ['dist'],
-	extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
+	extends: [
+		js.configs.recommended,
+		...tseslint.configs.recommended,
+		jsdoc.configs['flat/stylistic-typescript'],
+		prettier,
+	],
 	files: ['**/**.{ts,tsx}'],
 	languageOptions: {
 		ecmaVersion: 2020,
@@ -16,9 +22,11 @@ export default tseslint.config({
 	plugins: {
 		'react-hooks': reactHooks,
 		'react-refresh': reactRefresh,
+		jsdoc,
 	},
 	rules: {
 		...reactHooks.configs.recommended.rules,
+		'jsdoc/lines-before-block': 'off',
 		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 		'@typescript-eslint/no-unused-vars': [
 			'error',
