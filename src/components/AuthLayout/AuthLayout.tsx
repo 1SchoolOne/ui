71ad922c39eb44a6b37b9f6719f50f74006f5a21
IlhanYapici <ilhan.yapici@outlook.fr>
2024-10-29 21:54:22 +0100

    feat(components): add AuthLayout component

---
+++ src/components/AuthLayout/AuthLayout.tsx
@@ -0,0 +1,58 @@
+import { Col, Row } from 'antd'
+import { ReactNode } from 'react'
+
+import meshGradient from '@assets/brand-mesh-gradient.png'
+
+import './AuthLayout-styles.less'
+
+interface PanelSpan {
+	xs: number
+	sm: number
+	md: number
+	lg: number
+	xl: number
+	xxl: number
+}
+
+const leftPanelSpan: PanelSpan = {
+	xs: 0,
+	sm: 0,
+	md: 12,
+	lg: 12,
+	xl: 14,
+	xxl: 14,
+}
+
+const rightPanelSpan: PanelSpan = {
+	xs: 24,
+	sm: 24,
+	md: 12,
+	lg: 12,
+	xl: 10,
+	xxl: 10,
+}
+
+interface AuthLayoutProps {
+	children: ReactNode
+	heroPanel?: ReactNode
+}
+
+export function AuthLayout(props: AuthLayoutProps) {
+	const { heroPanel, children } = props
+
+	return (
+		<Row className="schoolone-auth-layout">
+			<Col className="schoolone-auth-layout__hero-panel" {...leftPanelSpan}>
+				<div className="schoolone-auth-layout__hero-panel__content">{heroPanel}</div>
+				<img
+					className="schoolone-auth-layout__hero-panel__bg"
+					src={meshGradient}
+					alt="brand mesh gradient"
+				/>
+			</Col>
+			<Col className="schoolone-auth-layout__main-panel" {...rightPanelSpan}>
+				{children}
+			</Col>
+		</Row>
+	)
+}
