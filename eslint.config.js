7ac7759deeaf807de27268a3a77aeb4b8758d691
IlhanYapici <ilhan-yapici@outlook.fr>
2025-04-29 11:17:20 +0200

    fix(eslint): run prettier as eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
-import js from '@eslint/js'
-import prettier from 'eslint-config-prettier'
+import eslint from '@eslint/js'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
 import jsdoc from 'eslint-plugin-jsdoc'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -7,14 +7,14 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config({
+	files: ['lib/**/*.{ts,tsx}'],
 	ignores: ['dist'],
 	extends: [
-		js.configs.recommended,
+		eslint.configs.recommended,
 		...tseslint.configs.recommended,
 		jsdoc.configs['flat/stylistic-typescript'],
-		prettier,
+		eslintPluginPrettier,
 	],
-	files: ['lib/**/*.{ts,tsx}'],
 	languageOptions: {
 		ecmaVersion: 2020,
 		globals: globals.browser,
